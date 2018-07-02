20b41976f140739b1fe1fd6afd4079759f04bf54
flatflax <present_xm@qq.com>
2018-07-02 20:12:12 +0800

    2018/7/2
    学会用blueprint了；解决了db的循环引用问题

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 # imports
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 import os
+import redis
+from models import db,Dashboard
 
 
 # grabs the folder where the script runs
@@ -18,6 +19,9 @@ SECRET_KEY = 'my_precious'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 
+# REDIS_HOST = '43.82.163.74'
+# REDIS_PORT = 6379
+
 # defines the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
@@ -25,20 +29,23 @@ DATABASE_PATH = os.path.join(basedir, DATABASE)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # 跟踪记录修改：False
 
+# REDIS = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+
 # create app
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
-db = SQLAlchemy(app)
 
-import models
+db.init_app(app)
 
+from routes import admin
+app.register_blueprint(admin.bp, url_prefix='/manage')
 
 @app.route('/')
 def index():
     """display the iframe"""
-    entries = models.Dashboard.query.all()
+    entries = Dashboard.query.all()
     return render_template('index.html', entries=entries)
 
 
@@ -74,87 +81,31 @@ def show_dashboard(post_id):
             flash('Please login first.')
             return redirect(url_for('login'))
         elif session['logged_in'] == True:
-            dashboard = models.Dashboard.query.filter_by(post_id=post_id).first()
-            entries = models.Dashboard.query.all()
+            dashboard = Dashboard.query.filter_by(post_id=post_id).first()
+            entries = Dashboard.query.all()
             if dashboard is None:
                 abort(404)
             return render_template('dashboard.html', dashboard=dashboard, entries=entries)
 
 
-@app.route('/manage')
-def manage():
-    """User manage link of iframe"""
+@app.route('/getShort')
+def getShortLink():
     if not session.get('logged_in'):
         flash('Please login first.')
         return redirect(url_for('login'))
-    elif session['logged_in'] == True:
-        entries = models.Dashboard.query.all()
-        return render_template('manage.html', entries=entries)
+    entries = Dashboard.query.all()
+    key = {}
+    key['longlink'] = ''
+    key['shortlink'] = ''
+    return render_template('getshort.html', entries=entries, key=key)  
 
-
-@app.route('/id/<post_id>', methods=['POST','DELETE'])
-def link_post_id(post_id):
-    """
-    POST : Add new link
-    PUT : Modify existed link
-    GET : Get more info the link
-    DELETE : Delete the link
-    """
-    from models import db
+@app.route('/link/<link_id>', methods=['POST','GET'])
+def ShortLink(link_id):
+    if not session.get('logged_in'):
+        flash('Please login first.')
+        return redirect(url_for('login'))
     if request.method == 'POST':
-        if request.form["_method"] == 'DELETE':
-            d = models.Dashboard.query.filter_by(post_id=post_id).first_or_404()
-            db.session.delete(d)
-            db.session.commit()
-            flash('Delete Success.')
-            return redirect(url_for('manage'))
-        if request.form["_method"] == 'PUT':
-            d = models.Dashboard.query.filter_by(post_id=post_id).first_or_404()
-            d.title = request.form["title"]
-            d.url = request.form["url"]
-            db.session.add(d)
-            db.session.commit()
-            flash('Update Success.')
-            return redirect(url_for('manage'))
-        else :
-            d = models.Dashboard(title=request.form["title"], url=request.form["url"])
-            db.session.add(d)
-            db.session.commit()
-            flash('Add Success.')
-            return redirect(url_for('manage'))
-    if request.method == 'PUT':
-        entry = models.Dashboard.query.filter_by(post_id=post_id).first().update(title=request.form["title"],url=request.form["url"])
-        flash('Update Success.')
-        return redirect(url_for('manage'))
-
-
-@app.route('/manage/update/<post_id>')
-def manage_update(post_id):
-    if not session.get('logged_in'):
-        flash('Please login first.')
-        return redirect(url_for('login'))
-    elif session['logged_in'] == True:
-        entry = models.Dashboard.query.filter_by(post_id=post_id).first()
-        # set key:Update
-        key = {}
-        key['value'] = 'Update'
-        key['method'] = 'PUT'
-        return render_template('linkinfo.html', entry=entry, key=key)
-
-
-@app.route('/manage/add')
-def manage_add():
-    if not session.get('logged_in'):
-        flash('Please login first.')
-        return redirect(url_for('login'))
-    elif session['logged_in'] == True:
-        # create new blank entry
-        entry = models.Dashboard(title='',url='')
-        # set key:Add
-        key = {}
-        key['value'] = 'Add'
-        key['method'] = 'POST'
-        return render_template('linkinfo.html', key=key, entry=entry)
+        pass
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,5 +1,4 @@
-from app import db
-
+from exts import db
 
 class Dashboard(db.Model):
 
